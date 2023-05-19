800b0a18541fa63226d5621315d0928f74edc272
Mohammed Husamuddin <uni.husam@gmail.com>
2023-05-19 22:02:33 +0400

    V1

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def main():
+    st.title('Sustainable and Renewable Resources')
+    st.image('Clean-energy.png', use_column_width=True)
+
+    st.markdown('''
+        Sustainable and renewable resources are crucial for a greener and more sustainable future. These resources are
+        naturally replenished and have a lower impact on the environment compared to non-renewable resources such as fossil fuels.
+        They include solar energy, wind energy, hydropower, geothermal energy, and bioenergy.
+
+        By harnessing sustainable and renewable resources, we can reduce greenhouse gas emissions, combat climate change, and
+        create a cleaner and healthier planet for future generations.
+
+        This website aims to provide information and raise awareness about sustainable and renewable resources. Explore the
+        different types of resources and their benefits to gain a better understanding of their importance in creating a
+        sustainable future.
+
+        Get involved and take action towards a more sustainable world today!
+    ''')
+
+if __name__ == '__main__':
+    main()
